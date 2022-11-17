e2c480bf4bfce6ae5d556253442c9eca48d96471
DenaldM <dmetuku@gmail.com>
2022-11-17 14:36:28 +0100

    finishing the UI of the project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import ParticlesBg from 'particles-bg';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/*<FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
